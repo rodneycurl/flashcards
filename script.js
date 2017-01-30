30af655ffd855c058b7a629411829c95bf46c8b0
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-30 17:29:58 -0500

    all of my files

---
+++ script.js
@@ -0,0 +1,14 @@
+// build a flash-cards flip game that marks when the player got the flash-card right or wrong (Based on Michael Jordan).
+// build a landing page "Which Jordan Did What?" add a "Play Now" button
+// if the player gets the flash-card correct, congratulate them (alert/prompt). Then reveal a button that will allow the player to move on to the next question.
+// if the player gets the flash-card wrong, notify them they are wrong and reveal the answer (alert/prompt). Then reveal a button that will allow the player to move on to the next question.
+// create a headline introducing the question. add an image under the <h1>. write the question in <h2> or <p>. Build an input field for the player to enter their answer with a "submit" button.
+// output a jordan for each level depending on the number of questions you get correct.
+// ask 5 questions total.
+// Reveals: 1 Correct Answers, Michael Jordan, Wizards -m "Just go sit down somewhere man you old playa!"
+// Reveals: 2 Correct Answers, Michael Jordan, Langley High School -m "C'mon man you still got to learn the game!"
+// Reveals: 3 Correct Answers, Michael Jordan, GM of Bob Cats -m "You got a long way to go playa!"
+// Reveals: 4 Correct Answers, Michael Jordan, UNC Chapel Hill -m "Yeah You the man but the best is yet to come!"
+// Reveals: 5 Correct Answers, Michael Jordan, Bulls -m "Your a God when it comes to this game, but can you three-peat?"
+// Randomize the responses whether right or wrong. If a person guess a wrong answer they'll get a gif from the 'wrong answer folder'. If a person guess right they'll get a gif from the 'right answer' folder.
+// Find out whether or not the player is a true Michael Jordan enthusiast/fan.
