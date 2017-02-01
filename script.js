eb96d707457d25fed09b5da3451002b57508a5b8
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-01 09:04:08 -0500

    uncommited the extra questions for my code

--- script.js
+++ script.js
@@ -35,47 +35,91 @@ var questionList = [
       id: 3,
       text:'What team did MJ win 6 NBA finals with?',
       answer: 'Chicago Bulls'
-    },
-    {
-      id: 4,
-      text:'What\'s the name of the movie Michael Jordan starred in with Bugs Bunny?',
-      answer: 'Space Jam'
-    },
-    {
-      id: 5,
-      text:'What MLB Baseball team did MJ play for?',
-      answer: 'White Soxs'
-    },
-    {
-      id: 6,
-      text:'What college did Michael Jordan attend?',
-      answer: 'UNC Chapel Hill'
-    },
-    {
-      id: 7,
-      text:'What shoes did MJ wear in the flu game?',
-      answer: 'Air Jordan 12 Black/Red'
-    },
+    }
+    // },
+    // {
+    //   id: 4,
+    //   text:'What\'s the name of the movie Michael Jordan starred in with Bugs Bunny?',
+    //   answer: 'Space Jam'
+    // },
+    // {
+    //   id: 5,
+    //   text:'What MLB Baseball team did MJ play for?',
+    //   answer: 'White Soxs'
+    // },
+    // {
+    //   id: 6,
+    //   text:'What college did Michael Jordan attend?',
+    //   answer: 'UNC'
+    // },
+    // {
+    //   id: 7,
+    //   text:'What shoes did MJ wear in the flu game?',
+    //   answer: 'Air Jordan 12 Black/Red'
+    // },
 ]
 
-var currentQuestion = 0
+var currentQuestion=0;
 
 // REMEMBER $('.message').html('<h1> You Won </h1>') //// $('.message').html()
 
+var score = 0;
+var output;
+
+//Created a variable for the score
+
 function beginGame() {
+  var currentQuestion = 0;
   //instead of console log, this is where you would use jquery to change the text/images on DOM
-  console.log(questionList[currentQuestion].text);
+  // console.log(questionList[currentQuestion].text);
   $(".questionsList").html( questionList[currentQuestion].text );
-  console.log('begin');
+  // console.log('begin');
 }
 
-  $(".inputanswersubmit").click(nextQuestion)
+  $(".inputanswersubmit").click(function(){
+    //get user input
+    var userinput = $(".input4answer").val();
+    //check user input against the answer
+    if (userinput == questionList[currentQuestion].answer){
+          output = "Correct";
+          score++;
+    } else {
+      output = "Wrong";
+    }
+    $(".alert").css("display", "block");
+    $(".answer").css("display", "block");
+    $(".alert").html(output);
+    $(".answer").html("The Answer Is: "+questionList[currentQuestion].answer);
+    $(".scoreboard").html("SCORE: "+score);
+    //if right, congratulate the player
+    //else, let the player know that they guessed wrong
+    $(".next").css("display", "block");
+  });
+$(".next").click(nextQuestion);
 
 function nextQuestion(){
   //instead of console log, this is where you would use jquery to change the text/images on DOM
-  currentQuestion++
-  $(".questionsList").html( questionList[currentQuestion].text );
-  console.log($(".questionsList").html())
+
+  console.log(currentQuestion);
+  console.log(questionList.length);
+  if (currentQuestion == (questionList.length-1)){
+    output = "Game Over";
+    $(".alert").html(output);
+    $(".answer").css("display", "none");
+    // $(".playagain").css("display", "block");
+    $(".next").css("display", "none");
+
+  } else {
+    currentQuestion++;
+    $(".questionsList").html( questionList[currentQuestion].text );
+    $(".alert").css("display", "none");
+    $(".answer").css("display", "none");
+    $(".input4answer").val("");
+  }
 }
+
+//  $(".playagain").click(beginGame);
+
+$('.message').html('<h1> You Won </h1>')
 beginGame()
 });
